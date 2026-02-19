8c4ecdaa1b68daccb53e61e62e8989ffb9cffb47
Eduarda Leal <edudardaleal753@gmail.com>
2026-02-19 20:44:51 -0300

    Adiciona edição de cor e de logo de cada loja

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,12 @@ class ProductController extends Controller
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
             'description' => 'nullable|string',
+            'image' => 'nullable|image|max:2048'
         ]);
+
+        if ($request->hasFile('image')) {
+            $validated['image_url'] = $request->file('image')->store('products', 'public');
+        }
 
 
         Product::create($validated);
